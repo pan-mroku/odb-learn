4927c19857b2b9e78378746caf632c2d9ff0372a
Paweł Szymański <out.of.logins.error@gmail.com>
2012-12-08 02:41:36 +0100

    create_add_save Tworzy nową bazę i dodaje elementy.

--- create_add_save.cpp
+++ create_add_save.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
-#include <odb/database.hxx>
+//#include <odb/database.hxx>
 
 //sqlite
-#include <odb/connection.hxx>
+//#include <odb/connection.hxx>
 #include <odb/transaction.hxx>
 #include <odb/schema-catalog.hxx>
 #include <odb/sqlite/database.hxx>
 
+#include "person.hpp"
+#include "person-odb.hxx"
+
 int main()
 {
-  odb::sqlite::database db("test");
+  odb::sqlite::database db("test.db", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
+
+  {
+    person john ("John", "Doe", 31);
+    person jane ("Jane", "Doe", 29);
+
+    odb::core::transaction t (db.begin ());
+    odb::schema_catalog::create_schema (db);
+    db.persist(john);
+    db.persist(jane);
+    t.commit();
+  }
+
+  {
+    odb::core::transaction t (db.begin ());
+
+    odb::result<person> r (db.query<person>());
+
+    odb::result<person>::iterator i (r.begin ());
+
+    for (odb::result<person>::iterator i (r.begin ()); i != r.end (); ++i)
+      std::cout << "Hello, " << i->first () << " " << i->last () << "!" << std::endl;
+
+
+    t.commit ();
+  }
+
+
   std::cout<<db.name()<<std::endl;
   return 0;
 }
